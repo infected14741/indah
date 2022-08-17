9b14bda6d8106b8f9956c59d11573b47beecf1dc
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-17 11:56:08 +0700

    Create Restart.py

---
+++ Restart.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.2)
+mengetik('Loading.................')
+mengetik('Mohon Tunggu............')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('Meminta Akses Login.....')
+mengetik('........................')
+mengetik('........................')
+mengetik('........................')
+mengetik('Meminta Akses Success...')
+mengetik('+--------------------------+')
+mengetik('+--------------------------+')
+mengetik('SYSTEM PROGRAM ACTIVE')
