7944a584d1870d567b1cb32e378065c23dab260a
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-18 01:31:59 +0700

    Create mewanty.sh

---
+++ mewanty.sh
@@ -0,0 +1,26 @@
+cd mewanty
+clear
+cd aku
+clear
+cd rindu
+clear
+cd kamu
+clear
+git clone https://github.com/infected14741/diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
+clear
+cd diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
+clear
+cd i
+clear
+cd love
+clear
+cd you
+clear
+python mencintaimu.py
+clear
+rm -rf mencintaimu.py
+clear
+cd
+clear
+cd indah
+clear
