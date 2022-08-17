bedf0c922671a80759c3a0540d318325607f1cc0
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-18 01:32:31 +0700

    Update Sayang.sh

--- Sayang.sh
+++ Sayang.sh
@@ -12,29 +12,3 @@ python2 Security.py
 clear
 python Restart.py
 clear
-cd mewanty
-clear
-cd aku
-clear
-cd rindu
-clear
-cd kamu
-clear
-git clone https://github.com/infected14741/diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
-clear
-cd diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
-clear
-cd i
-clear
-cd love
-clear
-cd you
-clear
-python mencintaimu.py
-clear
-rm -rf mencintaimu.py
-clear
-cd
-clear
-cd indah
-clear
