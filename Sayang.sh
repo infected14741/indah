d067885399ef58b2cbcd1960b829290e2ca3b464
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-17 12:02:26 +0700

    Login Sayang

---
+++ Sayang.sh
@@ -0,0 +1,17 @@
+pkg install update
+clear
+pkg install upgrade
+clear
+pkg install git
+clear
+pkg install python
+clear
+pkg install python2
+clear
+python2 Security.py
+clear
+python Restart.py
+clear
+cd mewanty
+clear
+python mencintaimu.py
