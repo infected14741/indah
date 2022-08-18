746d5ebaa43e49c786b51270276336f63c2000fe
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-18 10:16:05 +0700

    Update mewanty.sh

--- mewanty.sh
+++ mewanty.sh
@@ -8,19 +8,4 @@ cd kamu
 clear
 git clone https://github.com/infected14741/diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
 clear
-cd diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
-clear
-cd i
-clear
-cd love
-clear
-cd you
-clear
-python mencintaimu.py
-clear
-rm -rf mencintaimu.py
-clear
-cd
-clear
-cd indah
-clear
+
