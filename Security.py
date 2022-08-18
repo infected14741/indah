175e3ac1d2d9248d75610fc8e4d982736dc88b35
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-18 13:49:14 +0700

    Create Security.py

---
+++ Security.py
@@ -0,0 +1,67 @@
+import os, sys
+
+print ("\033[1;32mSYSTEM LOGIN INSTAGRAM DARK ATTACK HIGHLIGHT")
+
+print ("\033[1;32mMASUKKAN USER & PASSWORD")
+
+username = 'Infected_AOFSFOF'      
+
+password = 'MENCINTAIMU DALAM DIAM'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mLogin SUCCESS...", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\032[1;32mPASSWORD YANG ANDA MASUKAN SALAH...!!!\033[00m"
+
+			print "Back Login\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mSORRY USER ANDA SALAH...!!!\033[00m"
+
+		print "Back Login\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
